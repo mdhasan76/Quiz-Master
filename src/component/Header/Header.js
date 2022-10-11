d9978e59e7e77018d4603d18751516c8ce3fe83c
Md Hasan <mdhasan8064@gmail.com>
2022-10-11 23:16:10 +0600

    create dinamic link

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -3,10 +3,10 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <section className=' shadow-lg py-3'>
+        <section className=' shadow-lg py-3 bg-white sticky top-0'>
             <nav className='flex justify-between max-w-7xl mx-auto px-4'>
                 <div>
-                    <h1 className='text-3xl font-bold text-cyan-400'><span className='text-pink-400'>Q</span>uize Master </h1>
+                    <h1 className='text-3xl font-bold text-cyan-500'><span className='text-pink-400'>Q</span>uiz Master </h1>
                 </div>
                 <div>
                     <Link className='text-lg font-medium px-2' to={'/topics'}>Topics</Link>
