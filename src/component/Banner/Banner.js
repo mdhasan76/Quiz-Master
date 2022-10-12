416130cb5fc46f2fb24e74304488a2c445706ec9
Md Hasan <mdhasan8064@gmail.com>
2022-10-12 21:56:03 +0600

    responsive

--- src/component/Banner/Banner.js
+++ src/component/Banner/Banner.js
@@ -4,7 +4,7 @@ const Banner = () => {
     return (
         <div className='bg-banner-img py-[100px] lg:py-[170px] bg-cover bg-no-repeat'>
             {/* <img className='h-[500px] w-full' src={banner} alt="" /> */}
-            <div className='max-w-2xl lg:px-10 '>
+            <div className='max-w-2xl px-7 lg:px-10 '>
                 <h2 style={{ 'lineHeight': '1.5' }} className=' text-white font-bold  text-3xl md:text-4xl lg:text-5xl'>Give More and More quiz<br /> Build Better Confidence </h2>
                 <p className='text-lg opacity-70 font-medium py-3 text-white'>Giving exam make a man confindaceable. He can justify himself giving & facing more and more exam. so learn more give more quiz and build up your confidence</p>
             </div>
