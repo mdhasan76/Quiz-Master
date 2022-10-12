5098bc2d78e6e6937292d7fcf1ca9e1b6a24c3ee
Md Hasan <mdhasan8064@gmail.com>
2022-10-12 18:46:56 +0600

    Toast added

--- src/component/Quiz/Question.js
+++ src/component/Quiz/Question.js
@@ -1,42 +1,23 @@
 import React from 'react';
-import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEye } from '@fortawesome/free-solid-svg-icons'
 
-const Question = ({ faq }) => {
-    // console.log(faq)
-    const [alertEye, setalertEye] = useState();
+// toast.configure()
 
-    const rightAnswer = () => {
-        setalertEye(correctAnswer)
-        // toast(correctAnswer)
-        alert(correctAnswer)
-    };
-    const { correctAnswer, question, options, id } = faq;
-    const validation = (params) => {
-        // console.log('clicked')
-        if (params === correctAnswer) {
-            alert(correctAnswer);
-        }
-        else {
-            alert('wrong anwser')
-        }
-    }
-    // const rightAnswer = () => {
-    //     console.log('eye button clicked')
-    // }
+const Question = ({ faq, rightAnswer, validation }) => {
+
+    const { correctAnswer, question, options } = faq;
 
     return (
         <div className='my-7 bg-cyan-100 py-4'>
             <div className='text-right px-5'>
-                <FontAwesomeIcon icon={faEye} onClick={() => rightAnswer()}></FontAwesomeIcon>
+                <FontAwesomeIcon icon={faEye} onClick={() => rightAnswer(correctAnswer)}></FontAwesomeIcon>
             </div>
             <p className='m-7 text-lg font-medium'> {question.slice(3, -4)}</p>
 
             {
-                options.map(option => <button className=" block w-10/12 mx-auto m-3 rounded p-4 bg-blue-500 text-white font-semibold text-lg hover:bg-blue-800" onClick={() => validation({ option }.option)}>  {option}</button>)
+                options.map(option => <button className=" block w-10/12 mx-auto m-3 rounded p-4 bg-blue-500 text-white font-semibold text-lg hover:bg-blue-800" onClick={() => validation({ option }.option, correctAnswer)}>  {option}</button>)
             }
-
         </div>
     );
 };
--- src/component/Quiz/Quiz.js
+++ src/component/Quiz/Quiz.js
@@ -1,18 +1,45 @@
 import React from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { useLoaderData } from 'react-router-dom';
 import Question from './Question';
 
 const Quiz = () => {
     const quizData = useLoaderData();
-    const { logo, name, questions } = quizData.data;
+    const { name, questions } = quizData.data;
+
+
+    //Eye Clicke Show right Answer 
+    const rightAnswer = (correctAnswer) => {
+        toast.success(`Correct answer is: ${correctAnswer}`,
+            { position: toast.POSITION.TOP_CENTER })
+    };
+
+
+    // Validation Right Answer
+    const validation = (params, correctAnswer) => {
+        // console.log(params)
+        if (params === correctAnswer) {
+            toast.success('Right Answer!!',
+                { position: toast.POSITION.TOP_CENTER });
+        }
+        else {
+            toast.error('wrong anwser',
+                { position: toast.POSITION.TOP_CENTER })
+        }
+    }
+
     return (
         <div>
-            <h2 className='text-center text-4xl font-medium'>{name}</h2>
+            <h2 className='text-center text-4xl font-medium my-4 py-5 bg-cyan-500 text-white'>{name}</h2>
+
             <div className='max-w-xl mx-auto'>
                 {
-                    questions.map(question => <Question key={question.id} faq={question} />)
+                    questions.map(question => <Question rightAnswer={rightAnswer} validation={validation} key={question.id} faq={question} />)
                 }
             </div>
+
+            <ToastContainer />
         </div>
     );
 };
